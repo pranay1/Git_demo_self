8a1a566f4688c0b31fd51ee407748034d6475649
pranay <guptapranay274@gmail.com>
2017-07-08 03:31:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/pranay/git_demo_self/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.pranay.git_demo_self;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+  // added on initial commit
+
+
+    }
+}
