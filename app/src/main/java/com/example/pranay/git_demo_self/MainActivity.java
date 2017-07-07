e8a2ec29c0d89b86fac94216f7382257d41720fc
pranay <guptapranay274@gmail.com>
2017-07-08 03:33:30 +0530

    second commit

--- app/src/main/java/com/example/pranay/git_demo_self/MainActivity.java
+++ app/src/main/java/com/example/pranay/git_demo_self/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
   // added on initial commit
-
+// added on second push or commit
 
     }
 }
